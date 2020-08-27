0994f3874de8d1f9999c0c5d720e49c5933d56e0
Balazs Krizsan <krizsan.balazs@gmail.com>
2020-08-27 21:17:51 +0200

    Migration setup; run: \StackJudge\StackJudge.Ef>dotnet ef migrations add {NAME} --context AppDbContext --startup-project . --output-dir Migrations

---
+++ StackJudge.Ef/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace StackJudgeEf
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var option = new DbContextOptionsBuilder<AppDbContext>();
+            option.UseNpgsql("Host=localhost;Database=stuckjudge;Port=54320;Username=homestead;Password=secret;");
+
+            return new AppDbContext(option.Options);
+        }
+    }
+}
