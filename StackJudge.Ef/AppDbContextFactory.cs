30133c7463dc79bd31ca8b853495abc6ade6fd32
Krizsan Balazs <krizsan.balazs@gmail.com>
2020-10-03 00:49:08 +0200

    Typo fix

--- StackJudge.Ef/AppDbContextFactory.cs
+++ StackJudge.Ef/AppDbContextFactory.cs
@@ -8,7 +8,7 @@ namespace StackJudgeEf
         public AppDbContext CreateDbContext(string[] args)
         {
             var option = new DbContextOptionsBuilder<AppDbContext>();
-            option.UseNpgsql("Host=localhost;Database=stuckjudge;Port=54320;Username=homestead;Password=secret;");
+            option.UseNpgsql("Host=localhost;Database=stackjudge;Port=54320;Username=homestead;Password=secret;");
 
             return new AppDbContext(option.Options);
         }
